5b1b8b7e6da4c15ff10ef8ac6ae4a79d34aa9d7f
pritam samadder <pritamsamadder048@gmail.com>
2018-02-20 10:04:20 +0530

    commit on 20 02 2018 15 05 added scaling for any screen resolution

---
+++ CropMask/Assets/Scripts/CameraSetup.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraSetup : MonoBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        //Camera.main.aspect = 9f / 16f;
+
+        float targetAspectRatio = 9f / 16f;
+        float targetWidth = (float)Screen.width;
+        float targetHeight = (float)Screen.width / targetAspectRatio;
+
+        //Debug.Log(string.Format("targetWidth {0} , targetHeight {1}", targetWidth, targetHeight));
+
+        if (targetHeight > (float)Screen.height)
+        {
+            targetHeight = (float)Screen.height;
+            //targetHeight = Screen.height - 100f;
+            targetWidth = (float)Screen.height * targetAspectRatio;
+        }
+
+        //Debug.Log(string.Format("targetWidth {0} , targetHeight {1}", targetWidth, targetHeight));
+
+        float leftX = (Screen.width - targetWidth) / 2f;
+        float leftY = (Screen.height - targetHeight) / 2f;
+
+        //Debug.Log(string.Format("leftX {0} , leftY {1}", leftX, leftY));
+
+        //Rect pixelRect = new Rect(leftX, leftY, targetWidth, targetHeight);
+        Rect pixelRect = new Rect(leftX, 0, targetWidth, Screen.height);
+        Camera.main.pixelRect = pixelRect;
+    }
+}
